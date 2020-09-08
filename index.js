f2980938efd6b24b9affcc94a0c31d16da365a6a
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-09-08 14:37:32 +0300

    add: new  features

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 window.onload = function load() {
 
     let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,' '];
-
+    let timeout;
     function random(arr) {
     arr.pop()
     array = arr.sort(function(){
@@ -10,16 +10,74 @@ window.onload = function load() {
       });
     array.push(' ');
    document.body.removeChild(document.body.children[2]);
-
+    clearInterval(timeout);
     draw()
     go()
     }
 
+
+    function timer() {
+        let hour = '00';
+        let minutes = '00';
+        let counter = 0;
+        timeout = setInterval (function () {
+            counter++
+            let timer = document.querySelector('.timer');
+
+            if (counter === 60 && +minutes < 9) {
+                minutes = +minutes + 1;
+                minutes = '0' + minutes;
+                counter = 0;
+            } else if (counter === 60 && minutes >= 9) {
+                minutes = +minutes + 1;
+                counter = 0;
+            }
+
+            if (minutes === 60 && +hour < 9) {
+                hour = +hour + 1;
+                hour = '0' + hour;
+                minutes = 0;
+            } else if (minutes === 60 && hour >= 9) {
+                hour = +hour + 1;
+                minutes = 0;
+            }
+
+            if (counter < 10 ) {
+                timer.textContent = `${hour} : ${minutes} : 0${counter} `;
+            } else {
+            timer.textContent = `${hour} : ${minutes} : ${counter} `;
+            }
+
+        }, 1000)
+    }
+
     function buttons () {
+        let buttons = document.createElement('div');
+        buttons.classList.add('buttons')
+        document.body.prepend(buttons)
+
+        let random = document.createElement('div');
+        random.classList.add('start');
+        random.innerHTML = 'Перемешать'
+        buttons.prepend(random)
+
+        let time = document.createElement('div');
+        time.classList.add('start');
+        time.classList.add('timer');
+        time.innerHTML = 'Ваше время'
+        buttons.append(time)
+
         let start = document.createElement('div');
         start.classList.add('start');
-        start.innerHTML = 'Перемешать'
-        document.body.prepend(start)
+        start.classList.add('started');
+        start.innerHTML = 'Игра на время'
+        buttons.append(start)
+
+        let step = document.createElement('div');
+        step.classList.add('start');
+        step.classList.add('step');
+        step.textContent = 'Кол-во шагов'
+        buttons.append(step)
     }
 
     function draw() {
@@ -45,6 +103,12 @@ window.onload = function load() {
         random(array)
     })
 
+    let started = document.querySelector('.started')
+    started.addEventListener('click', function() {
+        random(array)
+        timer()
+    })
+
     function congratulated () {
         let arrayCell = document.querySelectorAll('.hole');
         let count = true;
@@ -54,12 +118,14 @@ window.onload = function load() {
             }
         }
         if (count) {
+            clearInterval(timeout);
             alert('Поздравляем! Вы победили')
         }
     }
     
-
     function go() {
+        let counter = 0;
+        let step = document.querySelector('.step');
     let cell = document.querySelectorAll('.hole');
     cell.forEach((div, index) => div.addEventListener('click',
      function () {
@@ -68,26 +134,31 @@ window.onload = function load() {
                 if(index !== 11 && index !== 3 && index !== 7) {
             cell[index + 1].textContent = cell[index].textContent;
             cell[index].textContent = ' ' ;
+            counter++
                 } 
          }
 
          if (cell[index + 4] !== undefined && cell[index + 4].textContent == ' ') {
             cell[index + 4].textContent = cell[index].textContent;
             cell[index].textContent = ' ';  
+            counter++
          }
 
          if (cell[index - 1] !== undefined && cell[index - 1].textContent == ' ') {
             if(index !== 4 && index !== 8 && index !== 12) {
                 cell[index - 1].textContent = cell[index].textContent;
                 cell[index].textContent = ' ';
+                counter++
             }  
          }
          
          if (cell[index - 4] !== undefined && cell[index - 4].textContent == ' ') {
             cell[index - 4].textContent = cell[index].textContent;
-            cell[index].textContent = ' ';  
+            cell[index].textContent = ' '; 
+            counter++ 
          }
          
+         step.textContent = counter;
          congratulated ()
         }))
     }
